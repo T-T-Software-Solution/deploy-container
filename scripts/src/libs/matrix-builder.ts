d81f15ed507fb1290b8c78f7e99fcd12731c8d03
Thada Wangthammang <thada.wth@gmail.com>
2025-01-24 22:24:58 +0700

    refactor: matrix Builder

--- scripts/src/libs/matrix-builder.ts
+++ scripts/src/libs/matrix-builder.ts
@@ -1,41 +1,6 @@
-
-export interface BuildDockerMatrix {
-  /**
-   * Repository
-   */
-  repository: string;
-  /**
-   * Branch
-   */
-  branch: string;
-  /**
-   * Image Name
-   */
-  image_name: string;
-  /**
-   * Image Tag
-   */
-  image_tag: string;
-  /**
-   * Working Directory
-   */
-  working_dir: string;
-  /**
-   * Build Arguments, JSON format, type: Record<string, string>
-   */
-  build_args: Record<string, string>;
-  /**
-   * Dockerfile
-   */
-  dockerfile: string;
-}
-
-export interface DeploymentMatrix {
-  docker_image: BuildDockerMatrix[];
-}
+import type { DeploymentMatrix } from "./types";
 
 export class MatrixBuilder {
-
 
   build(): DeploymentMatrix {
     return {
